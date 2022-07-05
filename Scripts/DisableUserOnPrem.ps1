2c62d6bcf888daa0d16c7c78271c007aa3526155
Jonathon Stufflebeam <64109456+jostuffl@users.noreply.github.com>
2022-07-05 14:12:27 -0400

    Create DisableUserOnPrem.ps1

---
+++ Scripts/DisableUserOnPrem.ps1
@@ -0,0 +1,50 @@
+<#
+  This is meant to run as an Azure Automation Runbook on a Hybrid Runbook worker, and it is paired with the Reset-Revoke-User Logic App.
+
+#>
+
+Param (
+
+[string] $UserAccounts
+
+)
+
+$myCredential = Get-AutomationPSCredential -Name 'HybridWorkerCred'
+
+Import-Module ActiveDirectory 
+
+foreach($User in $UserAccounts){
+try {
+
+	#Get SAM account name
+	$SAMAccountName = Get-ADUser -Credential $myCredential -Filter {UserPrincipalName -eq $User}
+
+	Write-Output "Finding and disabling user $SAMAccountName"
+
+	#Create Random Password
+	Write-Output "Creating random password..."
+	$Password = -join ((33..126) | Get-Random -Count 15 | ForEach-Object { [char]$_ })
+	
+
+    #Convert the password to secure string.
+	Write-Output "Converting to secure string"
+    $NewPwd = ConvertTo-SecureString $Password -AsPlainText -Force
+    
+	#Assign the new password to the user.
+	Write-Output "Resetting password.."
+    Set-ADAccountPassword -identity $SAMAccountName -NewPassword $NewPwd -Reset 
+
+    #Disable the user
+	Write-Output "Disabling User"
+	$SAMAccountName	| Disable-ADAccount 
+
+	Write-Output "Successfully disabled user account $SAMAccountName"
+}
+catch {
+    Write-Error "Error disabling user account $SAMAccountName"
+	write-error "$_"
+    throw $_
+    break
+}
+
+}
